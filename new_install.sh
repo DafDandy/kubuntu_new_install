de04a830cb82ed9b2e427088c561311095f7080a
DafDandy <102477185+DafDandy@users.noreply.github.com>
2023-02-13 12:44:02 -0800

    Update new_install.sh

--- new_install.sh
+++ new_install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash 
-echo "Update configuration file"
+echo "Update configuration file..."
 sleep 5
 sudo sed -i -e '$amax_parallel_downloads=10' /etc/dnf/dnf.conf
 sudo sed -i -e '$afastestmirror=True' /etc/dnf/dnf.conf
@@ -8,7 +8,7 @@ sudo dnf install https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-
 sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
 sudo dnf makecache
 sudo dnf install steam lutris akmod-nvidia xorg-x11-drv-nvidia-cuda
-echo "Updating OS"
+echo "Updating OS..."
 sleep 5
 sudo dnf update && sudo dnf upgrade -y
 echo "Rebooting sytem to apply changes 5..."
