dbca6e91226102a8435194edf4d0047b8cd16cda
DafDandy <102477185+DafDandy@users.noreply.github.com>
2023-02-13 12:15:34 -0800

    Update new_intall.sh

--- new_intall.sh
+++ new_intall.sh
@@ -2,11 +2,10 @@
 echo "Update configuration file"
 sudo sed -i -e '$amax_parallel_downloads=10' /etc/dnf/dnf.conf
 sudo sed -i -e '$afastestmirror=True' /etc/dnf/dnf.conf
-sudo dnf update && sudo dnf upgrade -y
-echo "Updating OS"
 sudo dnf groupinstall "KDE Plasma Workspaces"
 sudo dnf install https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
 sudo dnf makecache
 sudo dnf install steam lutris akmod-nvidia xorg-x11-drv-nvidia-cuda
-
+sudo dnf update && sudo dnf upgrade -y
+echo "Updating OS"
