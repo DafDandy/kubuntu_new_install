f83980c5e93228bd1bc23e0bf530794fd1120aa6
DafDandy <102477185+DafDandy@users.noreply.github.com>
2023-02-13 19:23:04 -0500

    Update new_install.sh

--- new_install.sh
+++ new_install.sh
@@ -6,6 +6,7 @@ sudo sed -i -e '$afastestmirror=True' /etc/dnf/dnf.conf
 sudo dnf groupinstall "KDE Plasma Workspaces"
 sudo dnf install https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 sudo dnf makecache
 sudo dnf install steam lutris akmod-nvidia xorg-x11-drv-nvidia-cuda
 sudo dnf groupupdate core
