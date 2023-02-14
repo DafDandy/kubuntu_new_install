622b0f82f586e707b9297b3eadd8a97f7774c362
DafDandy <102477185+DafDandy@users.noreply.github.com>
2023-02-13 20:22:07 -0500

    Update new_install.sh

--- new_install.sh
+++ new_install.sh
@@ -3,10 +3,13 @@ echo Choose your hostname to display in terminal:
 echo example: ryan@hostname
 read hostname
 hostnamectl set-hostname $hostname
-echo "Update configuration file..."
+echo "Updating configuration file..."
 sleep 3
 sudo sed -i -e '$amax_parallel_downloads=10' /etc/dnf/dnf.conf
 sudo sed -i -e '$afastestmirror=True' /etc/dnf/dnf.conf
+echo "Updating OS..."
+sleep 3
+sudo dnf update && sudo dnf upgrade -y
 sudo dnf groupinstall "KDE Plasma Workspaces"
 sudo dnf install https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
@@ -17,7 +20,6 @@ sudo dnf groupupdate core
 sudo dnf groupupdate sound-and-video
 echo "Updating OS..."
 sleep 3
-sudo dnf update && sudo dnf upgrade -y
 echo "Rebooting sytem to apply changes 5..."
 sleep 1
 echo "4..."
@@ -28,4 +30,5 @@ echo "2..."
 sleep 1
 echo "1..."
 sleep 1
+sudo dnf update && sudo dnf upgrade -y
 sudo reboot
