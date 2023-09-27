45188002cd3b97410a5c5201ac5cc817b2006b9c
DafDandy <ryanfoster771@gmail.com>
2023-09-27 16:00:30 -0400

    Adding additional install

--- new_install.sh
+++ new_install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash 
+#!/bin/bash
 
 # Changing the dnf file to speed up downloads and configure the fastest mirror
 echo "Updating configuration file..."
@@ -12,7 +12,7 @@ sleep 3
 sudo dnf update && sudo dnf upgrade -y
 
 # Swapping the gnome with kde desktop
-
+sudo dnf groupinstall "KDE Plasma Workspaces"
 
 # Rebooting the system to apply all changes
 echo "Rebooting sytem to apply changes 5..."
