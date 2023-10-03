99064deccc88f2931be8aaac3a28ee3fc4e9b6ee
DafDandy <102477185+DafDandy@users.noreply.github.com>
2023-10-03 14:16:17 -0400

    Update new_install.sh

--- new_install.sh
+++ new_install.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 
-# Changing the dnf file to speed up downloads and configure the fastest mirror
-echo "Updating configuration file..."
-sleep 3
-sudo sed -i -e '$amax_parallel_downloads=10' /etc/dnf/dnf.conf
-sudo sed -i -e '$afastestmirror=True' /etc/dnf/dnf.conf
-
 # Updating OS
 echo "Updating OS..."
 sleep 3
-sudo dnf update && sudo dnf upgrade -y
+sudo apt update && sudo apt upgrade -y
 
 # Swapping the gnome with kde desktop
-sudo dnf groupinstall "KDE Plasma Workspaces"
+sudo apt install aptitude
+aptitude install kde-standard
 
 # Rebooting the system to apply all changes
 echo "Rebooting sytem to apply changes 5..."
