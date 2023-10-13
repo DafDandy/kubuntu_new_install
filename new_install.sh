6164840fb533bdf273dad81c49817a65b8fb5b4d
DafDandy <102477185+DafDandy@users.noreply.github.com>
2023-10-13 09:47:14 -0400

    Update new_install.sh

--- new_install.sh
+++ new_install.sh
@@ -5,10 +5,6 @@ echo "Updating OS..."
 sleep 3
 sudo apt update && sudo apt upgrade -y
 
-# Swapping the gnome with kde desktop
-sudo apt install aptitude
-sudo aptitude install kde-standard
-
 # Rebooting the system to apply all changes
 echo "Rebooting sytem to apply changes 5..."
 sleep 1
