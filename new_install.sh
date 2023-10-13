cd6a48a874423305df7db62b1b80f7835f4f7a3c
DafDandy <102477185+DafDandy@users.noreply.github.com>
2023-10-13 09:48:17 -0400

    Update new_install.sh

--- new_install.sh
+++ new_install.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+# Installing Nala package manager
+sudo apt install nala
+
 # Updating OS
 echo "Updating OS..."
 sleep 3
-sudo apt update && sudo apt upgrade -y
+sudo nala update && sudo nala upgrade -y
 
 # Rebooting the system to apply all changes
 echo "Rebooting sytem to apply changes 5..."
